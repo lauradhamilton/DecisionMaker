20fb84f00901989e4a00cf2b574b449b8a15fb8f
Laura Hamilton <lauradhamilton@gmail.com>
2014-03-29 12:38:29 -0500

    More changes

--- gen/com/example/decisionmaker/R.java
+++ gen/com/example/decisionmaker/R.java
@@ -18,7 +18,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_decision=0x7f030000;
-        public static final int activity_welcome=0x7f030001;
+        public static final int activity_developer=0x7f030001;
+        public static final int activity_welcome=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
